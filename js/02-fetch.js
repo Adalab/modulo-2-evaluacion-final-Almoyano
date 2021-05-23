f52fc6266aa2733f23d5fc67f92c27038c5981e0
Almoyano <a.moyano.miguel@gmail.com>
2021-05-23 13:12:54 +0200

    Fix fetch with localStorage

--- js/02-fetch.js
+++ js/02-fetch.js
@@ -2,18 +2,18 @@ if (localStorage.getItem("data") === null) {
   fetch("http://api.tvmaze.com/shows")
     .then((response) => response.json())
     .then((data) => {
-      localStorage.setItem("shows", JSON.stringify(data));
+      localStorage.setItem("show", JSON.stringify(data));
       renderPalettes(data);
     });
 } else {
-  const data = JSON.parse(localStorage.getItem("shows"));
+  const data = JSON.parse(localStorage.getItem("show"));
   renderPalettes(data);
 }
 
 function renderPalettes(data) {
   paletteList.innerHTML = "";
 
-  for (const show of data.shows) {
+  for (const show of data) {
     paletteList.innerHTML += `<li class="palette-list-item js-card favorite">
           <h2 class="item-name">${show.name}</h2>
           <img class="item-imagen-box" src="${show.image.medium}" alt="${show.name}"
