040a5f2083b288fb1b514fca24a364781b8ef5e8
Almoyano <a.moyano.miguel@gmail.com>
2021-05-25 13:49:49 +0200

    Add reset favorites button

--- js/01-globals.js
+++ js/01-globals.js
@@ -5,6 +5,7 @@ const paletteList = document.querySelector(".js-palette-serie");
 const form = document.querySelector(".js-form");
 const searchInput = document.querySelector(".js-search");
 const favoriteSerie = document.querySelector(".js-favoriteserie");
+const resetButton = document.querySelector(".js-reset");
 
 let globalData = [];
 let favoritesSeries = [];
